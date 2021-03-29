6146ddd00f0354e311c0e48c373549b97bc2d6b1
jcj <jcj154248@naver.com>
2021-03-29 12:35:27 +0900

    슈팅 게임

---
+++ game.py
@@ -0,0 +1,14 @@
+print("갤러그")
+
+print("1. 발사 2. 왼쪽 3. 오른쪽")
+
+number = input("숫자를 입력하세요")
+
+print("입력한 숫자는 " , number)
+
+if int (number) == 1 :
+    print("발사")
+if int (number) == 2 :
+    print("왼쪽")
+if int (number) == 3 :
+    print("오른쪽")
